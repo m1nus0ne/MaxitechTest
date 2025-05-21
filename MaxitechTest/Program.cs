d946c2b78fbe4889e3e9765c4daf93157b0c6840
m1nus0ne <m1nus0ne@mail.ru>
2025-05-21 12:09:20 +0500

    solve task1

--- MaxitechTest/Program.cs
+++ MaxitechTest/Program.cs
@@ -1,3 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Text;
 
-Console.WriteLine("Hello, World!");
+namespace MaxitechTest;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var input = Console.ReadLine();
+        if (string.IsNullOrEmpty(input))
+            return;
+
+        var n = input.Length;
+        var result = new StringBuilder { Capacity = 2 * n };
+
+        if (n % 2 == 0)
+        {
+            var half = n / 2;
+            for (int i = half - 1; i >= 0; i--)
+                result.Append(input[i]);
+            for (int i = n - 1; i >= half; i--)
+                result.Append(input[i]);
+        }
+        else
+        {
+            for (int i = n - 1; i >= 0; i--)
+                result.Append(input[i]);
+            result.Append(input);
+        }
+
+        Console.WriteLine(result);
+    }
+}
